0266e80e8841b1ba3e2950d17af8f921e016ba46
Pål-Erik Martinsen <palmarti@cisco.com>
2013-11-25 11:59:28 +0100

    Buiilding pages..

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+git checkout -- Drafts
+
+cat start.html > index.html
+rm content.html
+
+cd Drafts
+
+for FILE in `ls -l`
+do
+    if test -d $FILE
+    then
+      echo "Entering Directory: $FILE"
+      cd $FILE
+      for HTMLFILE in `ls *.html 2>/dev/null`
+      do
+          echo "Procsessing file: $HTMLFILE"
+          echo "<h3> $FILE </h3>" >> ../../content.html
+          echo "<p>" >> ../../content.html
+          echo "${HTMLFILE%%.*} <a href=Drafts/$FILE/$HTMLFILE> [html] </a>" >> ../../content.html
+          echo "<a href=Drafts/$FILE/${HTMLFILE%%.*}.txt> [txt] </a>" >> ../../content.html
+          echo "<a href=Drafts/$FILE/${HTMLFILE%%.*}.raw.txt> [raw.txt] </a>" >> ../../content.html
+          echo "</p>" >> ../../content.html
+          
+
+      done
+      cd ..
+    fi
+done
+cd ..
+
+cat content.html >>index.html
+cat end.html >>index.html
+
+rm -rf Drafts
